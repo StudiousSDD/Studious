bc9b60ab5831d18e18d6e814fc347c1b9b41ba23
Lorenzo Mercado <135887247+SirBirbly@users.noreply.github.com>
2023-07-17 14:28:37 -0400

    Added basic models

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    name_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name_text
+
+
+class Meeting(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    meeting_number = models.IntegerField(default=0)
+    def __str__(self):
+        title = "Meeting #{}"
+        return title.format(self.meeting_number)
+
+class Note(models.Model):
+    meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE)
+    note_title = models.CharField(max_length=200)
+    note_body = models.TextField()
+    def __str__(self):
+        return self.note_title
