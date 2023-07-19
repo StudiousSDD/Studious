d6fc0125791f8cc39bd522703b170c844bd90ea2
JoseIdrovo <114527497+JoseIdrovo@users.noreply.github.com>
2023-07-18 23:36:10 -0400

    Notes Feature

--- Studious/urls.py
+++ Studious/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from notes.views import editor
 
 urlpatterns = [
     path("", include("notes.urls")),
     path('admin/', admin.site.urls),
+    path('notes/',editor, name='editor'),
 ]
