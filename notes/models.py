111e598861b26356054c007782f1e368c423dfbf
Lorenzo Mercado <135887247+SirBirbly@users.noreply.github.com>
2023-08-09 14:29:33 -0400

    Comments

--- notes/models.py
+++ notes/models.py
@@ -6,7 +6,11 @@ from ckeditor.widgets import CKEditorWidget
 from schedule.models.events import Event, Occurrence
 
 # Create your models here.
-    
+
+# a Class needs an Event to show up on the calendar
+# a Class has a name and the date it was created
+# it is ordered alphabetically by name
+# it is displayed as it's name
 class Class(models.Model):
     calendar_event = models.OneToOneField(Event, on_delete=models.CASCADE, null=True)
 
@@ -19,6 +23,10 @@ class Class(models.Model):
     def __str__(self):
         return self.name
 
+# a Lecture needs an Occurence as it represents a single Class time
+# a Lecture is associated with a Class, each Class can have many Lectures
+# a Lecture has no name and is instead numbered
+# it is displayed as 'Class name' Lecture #'lecture_number' i.e. SD&D Lecture 1
 class Lecture(models.Model):
     occurrence = models.ForeignKey(Occurrence, on_delete=models.CASCADE, null=True)
     cls = models.ForeignKey(Class, on_delete=models.CASCADE, null=True)
@@ -28,7 +36,10 @@ class Lecture(models.Model):
         title = " Lecture #{}"
         return self.cls.__str__() + title.format(self.lecture_number)
     
-
+# a Note is assocciated with a Lecture, each Leture can have many Notes
+# A Note has a title, the text held within it, and dates of creation and modification
+# their natural sorting is alphabetical by title
+# a Note displays as 'Lecture display' 'Note title' i.e. SD&D Lecture 1 First Day Notes
 class Note(models.Model):
     lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE, null=True)
     
@@ -43,6 +54,8 @@ class Note(models.Model):
     def __str__(self):
         return self.lecture.__str__() + " " + self.title
 
+# an ArchivedNote is a way to hold the important information of a Note
+# it is held separately to be restored or deleted later
 class ArchivedNote(models.Model):
     lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE, null=True)
     
